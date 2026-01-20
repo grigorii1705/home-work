49b68a06a011c0e62f6a64db258ca07dcf58fbb4
grigorii1705 <washe.pravo@mail.ru>
2026-01-20 20:04:42 +0200

    19.01.26

--- Main.java
+++ Main.java
@@ -1,115 +1,221 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public static void main(String[] args)
-{
-  // Создаем объект Scanner для чтения данных с клавиатуры
-  //Scanner scanner = new Scanner(System.in);
+public static void main(String[] args) {
+    // Создаем объект Scanner для чтения данных с клавиатуры
+    //Scanner scanner = new Scanner(System.in);
 
-  // Считываем строку текста (пароль) с помощью метода nextLine()
-  //String password = scanner.nextLine();
+    // Считываем строку текста (пароль) с помощью метода nextLine()
+    //String password = scanner.nextLine();
 
-  // Выводим введенный пароль на экран
-  //System.out.println(password);
+    // Выводим введенный пароль на экран
+    //System.out.println(password);
 
-  // Закрываем сканер
-  //scanner.close();
+    // Закрываем сканер
+    //scanner.close();
 
-  // Создаем объект Scanner для чтения данных
-  //Scanner scanner = new Scanner(System.in);
+    // Создаем объект Scanner для чтения данных
+    //Scanner scanner = new Scanner(System.in);
 
-  // 1. Запрашиваем и считываем имя игрока (строка)
-  //String name = scanner.nextLine();
+    // 1. Запрашиваем и считываем имя игрока (строка)
+    //String name = scanner.nextLine();
 
-  // 2. Запрашиваем и считываем возраст игрока (целое число)
-  //int age = scanner.nextInt();
+    // 2. Запрашиваем и считываем возраст игрока (целое число)
+    //int age = scanner.nextInt();
 
-  // 3. Выводим сначала возраст
-  //System.out.println(age);
+    // 3. Выводим сначала возраст
+    //System.out.println(age);
 
-  // 4. Затем выводим имя
-  //System.out.println(name);
+    // 4. Затем выводим имя
+    //System.out.println(name);
 
-  // Закрываем сканер
-  //scanner.close();
+    // Закрываем сканер
+    //scanner.close();
 
-  // Создаем объект Scanner для чтения данных с клавиатуры
-  //Scanner scanner = new Scanner(System.in);
+    // Создаем объект Scanner для чтения данных с клавиатуры
+    //Scanner scanner = new Scanner(System.in);
 
-  // Считываем цену первого товара (целое число)
-  //int price1 = scanner.nextInt();
+    // Считываем цену первого товара (целое число)
+    //int price1 = scanner.nextInt();
 
-  // Считываем цену второго товара (целое число)
-  //int price2 = scanner.nextInt();
+    // Считываем цену второго товара (целое число)
+    //int price2 = scanner.nextInt();
 
-  // Вычисляем общую стоимость
-  //int total = price1 + price2;
+    // Вычисляем общую стоимость
+    //int total = price1 + price2;
 
-  // Выводим результат на экран
-  //System.out.println(total);
+    // Выводим результат на экран
+    //System.out.println(total);
 
-  // Закрываем сканер
-  //scanner.close();
+    // Закрываем сканер
+    //scanner.close();
 
-  // Создаем объект Scanner для чтения данных с клавиатуры
-  //Scanner scanner = new Scanner(System.in);
+    // Создаем объект Scanner для чтения данных с клавиатуры
+    //Scanner scanner = new Scanner(System.in);
 
-  // Считываем имя пользователя (строка)
-  //String name = scanner.nextLine();
+    // Считываем имя пользователя (строка)
+    //String name = scanner.nextLine();
 
-  // Считываем возраст пользователя (целое число)
-  //int age = scanner.nextInt();
+    // Считываем возраст пользователя (целое число)
+    //int age = scanner.nextInt();
 
-  // Выводим готовую строку с подставленными значениями
-  //System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
+    // Выводим готовую строку с подставленными значениями
+    //System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
 
-  // Закрываем сканер
-  //scanner.close();
+    // Закрываем сканер
+    //scanner.close();
 
-  // Объявляем переменную visitorAge и присваиваем ей значение 17
-  //int visitorAge = 17;
+    // Объявляем переменную visitorAge и присваиваем ей значение 17
+    //int visitorAge = 17;
 
-  // Используем оператор if-else для проверки возраста
-  //if (visitorAge < 18) {
+    // Используем оператор if-else для проверки возраста
+    //if (visitorAge < 18) {
     // Если возраст меньше 18, выводим сообщение о запрете
     //System.out.println("Доступ запрещен");
-  //} else {
+    //} else {
     // В противном случае выводим приветствие
     //System.out.println("Добро пожаловать!");
 // Объявляем переменную currentTemperature и присваиваем ей значение -5
-  //int currentTemperature = -5;
+    //int currentTemperature = -5;
 
-  // Используем оператор if-else для проверки температуры
-  //if (currentTemperature < 0) {
+    // Используем оператор if-else для проверки температуры
+    //if (currentTemperature < 0) {
     // Если температура меньше 0, выводим сообщение о морозе
     //System.out.println("Оденьтесь потеплее, на улице мороз!");
-  //} else {
+    //} else {
     // В противном случае сообщаем, что можно идти в легкой одежде
-   // System.out.println("Можно идти в легкой одежде!");
+    // System.out.println("Можно идти в легкой одежде!");
 
-  // Объявляем переменную userPassword и присваиваем ей значение "qwerty"
-  //String userPassword = "qwerty";
+    // Объявляем переменную userPassword и присваиваем ей значение "qwerty"
+    //String userPassword = "qwerty";
 
-  // Используем оператор if без else для проверки пароля
-  // Для сравнения строк в Java используется метод .equals()
-  //if (userPassword.equals("qwerty")) {
+    // Используем оператор if без else для проверки пароля
+    // Для сравнения строк в Java используется метод .equals()
+    //if (userPassword.equals("qwerty")) {
     // Если условие верно, выводим сообщение
     //System.out.println("Вход выполнен успешно");
 
-  // Объявляем переменную currentHour и присваиваем ей значение 20
-  //int currentHour = 20;
+    // Объявляем переменную currentHour и присваиваем ей значение 20
+    //int currentHour = 20;
 
-  // Используем оператор if-else if-else для выбора приветствия
-  //if (currentHour < 12) {
+    // Используем оператор if-else if-else для выбора приветствия
+    //if (currentHour < 12) {
     // Если значение меньше 12
     //System.out.println("Доброе утро!");
-  //} else if (currentHour <= 18) {
+    //} else if (currentHour <= 18) {
     // Если значение от 12 до 18 включительно
     //System.out.println("Добрый день!");
-  //} else {
+    //} else {
     // Во всех остальных случаях
     //System.out.println("Добрый вечер!");
+    // Конференция
+    //Scanner scanner = new Scanner(System.in);
 
+    // Ввод данных
+    //System.out.print("Введите ваш возраст: ");
+    //int age = scanner.nextInt();
 
-    }
+    // Очистка буфера после ввода числа
+    //scanner.nextLine();
+
+    //if (age >= 21) {
+    //  System.out.print("Введите код приглашения: ");
+    // String code = scanner.nextLine();
+
+    // Проверка кода
+    //if (code.equalsIgnoreCase("VIP")) {
+    //  System.out.println("Доступ к VIP-залу разрешен.");
+    //} else if (code.equalsIgnoreCase("GUEST")) {
+    //  System.out.println("Гостевой доступ разрешен.");
+    //} else {
+    //  System.out.println("Неверный код приглашения.");
+    //}
+    //} else {
+    // Если возраст меньше 21
+    //  System.out.println("К сожалению, только с 21 года.");
+//}
+//    scanner.close();
+   // Фейсконтроль
+   // Scanner scanner = new Scanner(System.in);
+
+    //System.out.print("Введите ваш возраст: ");
+    //int age = scanner.nextInt();
+
+    //scanner.nextLine(); // Очистка буфера
+
+    //System.out.print("Введите секретный код: ");
+    //String code = scanner.nextLine();
+
+    // Проверяем первое условие (возраст)
+    //if (age >= 18) {
+        // Если возраст подошел, проверяем второе условие (код)
+      //  if (code.equals("OPEN")) {
+        //    System.out.println("Вход разрешен!");
+        //} else {
+            // Если возраст ок, но код неверный
+          //  System.out.println("Вам сегодня не повезло, вход запрещен");
+        //}
+    //} else {
+        // Если возраст меньше 18
+        //System.out.println("Вам сегодня не повезло, вход запрещен");
+   // }
+
+    //scanner.close();
+//}
+    //MINSK
+    //Scanner scanner = new Scanner(System.in);
+
+    // Ввод возраста
+   // System.out.print("Введите ваш возраст: ");
+   // int age = scanner.nextInt();
+
+    // Очистка буфера (чтобы прочитать строку после числа)
+    //scanner.nextLine();
+
+    // Ввод города
+    //System.out.print("Введите ваш город: ");
+    //String city = scanner.nextLine();
+
+    // Логика без использования &&
+    //if (age >= 21) {
+        // Если возраст подходит, проверяем город
+        //if (city.equalsIgnoreCase("Минск")) {
+           // System.out.println("Добро пожаловать в клуб, Минск!");
+        //} else {
+            // Если возраст 21+, но город не Минск
+           // System.out.println("Извините, вход запрещен.");
+       // }
+ //   } else {
+ //       // Если возраст меньше 21
+ //       System.out.println("Извините, вход запрещен.");
+ //   }
+
+//    scanner.close();
+//    boolean isJavaFun = true;
+//    System.out.println(isJavaFun);
+//    int ourScore = 3;
+//    int opponentScore = 1;
+
+//    boolean weWon = ourScore > opponentScore;
+
+//    System.out.println(weWon);
+    // Объявление исходных переменных
+//    boolean isSunny = true;
+//    boolean isWeekend = true;
+
+    // canGoToPark будет true, только если оба условия верны
+//    boolean canGoToPark = isSunny && isWeekend;
+
+    // canStayHome будет true, если не солнечно ИЛИ если не выходной
+//    boolean canStayHome = !isSunny || !isWeekend;
+
+    // Вывод результатов на экран
+//    System.out.println(canGoToPark);
+//    System.out.println(canStayHome);
+//    int roomTemperature = 22; // Можно установить любое значение
+
+    // Проверка диапазона от 20 до 25 включительно
+//    boolean isComfortable = roomTemperature >= 20 && roomTemperature <= 25;
+
+//    System.out.println(isComfortable);
 }
 
